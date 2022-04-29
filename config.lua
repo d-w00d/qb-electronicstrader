d9d3942cf64c0342b284bc3eeb2240b6ffc01c74
realSixGrams <96337767+realSixGrams@users.noreply.github.com>
2022-04-29 02:35:00 -0400

    Configurable PED Modulation
    Menu Trading System will become Modular shortly...

--- config.lua
+++ config.lua
@@ -7,3 +7,12 @@ Config.Notifications = 'okok'
 Config.Logs = true
  -- True = Logs Enabled
  -- False = Logs Disabled
+
+Config.Peds = {
+    [1] = { -- Copy the template and add it below other entries to add as many peds as you would like.
+        coords = vector3(1126.3740, -471.7325, 66.4872), -- Coordinates for the PED to spawn
+        heading = 76.4781, -- Heading where the ped is facing
+        ped = 'cs_jimmydisanto', -- Ped Hash (https://docs.fivem.net/docs/game-references/ped-models/)
+        PedName = "Jimmy", -- Ped Name
+    },
+}
