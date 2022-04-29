6ec1eff7a898f44876f531d42bd8ae9572c3ca3c
realSixGrams <96337767+realSixGrams@users.noreply.github.com>
2022-04-29 02:22:54 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ author 'Constant Development'
 description 'QBCore Trader System'
 version '1.0.1'
 
-shared_script'config.lua'
+shared_script 'config.lua'
 
 client_script 'client/main.lua'
 server_script 'server/main.lua'
