884e799e3e416b640648397b7e87862862ec553a
realSixGrams <96337767+realSixGrams@users.noreply.github.com>
2022-04-29 02:35:51 -0400

    Modular PED/Target System
    This also relates to it no longer requiring ms-peds

--- client/main.lua
+++ client/main.lua
@@ -79,19 +79,52 @@ AddEventHandler('cr-trader:client:TradePrevention', function()
 end)
 
 Citizen.CreateThread(function()
-    exports['qb-target']:AddBoxZone("electronictrader", vector3(1126.31, -471.76, 66.49), 1, 1, {
-	name = "electronictrader",
-	heading = 345,
-	debugPoly = false,
-	}, {
-		options = {
-			{
-            	type = "client",
-            	event = "cr-trader:client:BaseElectronicsMenu",
-				icon = "fas fa-laptop-medical",
-				label = "Speak with Jeremy",
-			},
-		},
-		distance = 2.5
-    })
+    for k, v in pairs(Config.Peds) do
+        RequestModel(v.ped)
+        while not HasModelLoaded(v.ped) do
+            Wait(20)
+        end
+        local inRange = false
+        local spawned = v.spawned
+        while not spawned do
+            local ped = PlayerPedId()
+            local pedCoords = GetEntityCoords(ped)
+            local dist = #(pedCoords - v.coords)
+            
+            if dist <= 500 then
+                inRange = true
+            end
+    
+            if dist <= 100 then
+                local npc = CreatePed(0, v.ped, v.coords['x'], v.coords['y'], v.coords['z'], v.heading, true, 1)
+                ActivatePedTarget(npc, k)
+                v.spawned = true
+                break
+            end
+    
+            if not inRange then
+                Wait(2000)
+            end
+            Wait(100)
+        end
+        Wait(100)
+    end
 end)
+
+local function ActivatePedTarget(ped, k)
+    if Config.Target == "QB" then
+        exports['qb-target']:AddTargetEntity(ped, {
+            options = {
+                {
+                    type = "client",
+                    event = "cr-trader:client:BaseElectronicsMenu",
+                    icon = "fas fa-laptop-medical",
+                    label = "Speak with "..Config.Peds[k].PedName,
+                    ped = k
+
+                },
+            },
+            distance = 3.0
+        })
+    end
+end
